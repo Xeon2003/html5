5d9e12807a2b1c0c9d33bdf5c2c12e696f309e90
Jan Max Meyer jmm@phorward.de <jm@mausbrand.de>
2016-09-22 19:30:17 +0200

    Forgotten to add __init__.py

---
+++ lib/__init__.py
@@ -0,0 +1,46 @@
+from .textnode import TextNode
+from .widget import Widget
+from .body import Body
+from .div import Div
+from .table import Table,Tbody,Thead,Td,Th,Tr
+from .list import Ol, Ul, Li, Dl, Dt, Dd
+from .form import Button,Fieldset,Form,Input,Label,Optgroup,Option,Select,Textarea,Output
+from .nav import Nav
+from .span import Span
+from .base import Base
+from .a import A
+from .area import Area
+from .audio import Audio
+from .bdo import Bdo
+from .blockquote import Blockquote
+from .canvas import Canvas
+from .command import Command
+from .details import Details
+from .dialog import Dialog
+from .embed import Embed
+from .iframe import Iframe
+from .img import Img
+from .ins import Ins
+from .keygen import Keygen
+from .link import Link
+from .map import Map
+from .meta import Meta
+
+from .svg import Svg, SvgCircle, SvgEllipse, SvgG, SvgImage, SvgLine, SvgPath, SvgPolygon, SvgPolyline, SvgRect, SvgText
+
+from .meter import Meter
+from .object import Object
+from .param import Param
+
+from .q import Q
+from .progress import Progress
+
+from .source import Source
+from .style import Style
+from .time import Time
+from .track import Track
+
+from .video import Video
+from .elements import Abbr,Address,Article,Aside,B,Bdi,Br,Caption,Cite,Code,Datalist,Dfn,Em,Figcaption,Figure,Footer,Header,H1,H2,H3,H4,H5,H6,Hr,I,Kdb,Legend,Mark,Noscript,P,Rq,Rt,Ruby,S,Samp,Section,Small,Strong,Sub,Summery,Sup,U,Var,Wbr
+from ._del import _Del as Del
+from ._script import _Script as Script
