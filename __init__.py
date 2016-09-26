35f283e3789d3c28f675b80911de0ac49675e219
Jan Max Meyer jmm@phorward.de <jm@mausbrand.de>
2016-09-26 11:10:35 +0200

    Importing keycodes.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
 from .lib import *
+from .lib import keycodes
+
 from .ext import *
 from .utils import *
--- lib/__init__.py
+++ lib/__init__.py
@@ -44,3 +44,5 @@ from .video import Video
 from .elements import Abbr,Address,Article,Aside,B,Bdi,Br,Caption,Cite,Code,Datalist,Dfn,Em,Figcaption,Figure,Footer,Header,H1,H2,H3,H4,H5,H6,Hr,I,Kdb,Legend,Mark,Noscript,P,Rq,Rt,Ruby,S,Samp,Section,Small,Strong,Sub,Summery,Sup,U,Var,Wbr
 from ._del import _Del as Del
 from ._script import _Script as Script
+
+from .keycodes import *
